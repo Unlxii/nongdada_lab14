cb69cd51ad8741532b4854b82a6a2011cca2991b
Nongdada <ronnachai_si@cmu.ac.th>
2023-01-13 16:09:55 +0700

    "done"

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+
+    for(int i=0; i<N; i++)
+        B[0]+= A[i];
+    
+
+
+
+  B[0]=B[0]/N;
+    double summ = 0;
+  for(int j = 0; j < N; ++j) {
+    summ += pow(A[j] - B[0], 2);
+  }
+B[1] = sqrt(summ /N);
+    
+    double Geometric = 1;
+   for (int k = 0; k < N; k++){
+        Geometric = Geometric * A[k];
+   }
+   B[2] = pow(Geometric, pow(N,-1));
+
+    double harmonic;
+   for (int z = 0; z < N; z++){
+    harmonic = harmonic + (double)1/A[z];
+   }
+   B[3] = N/harmonic;
+
+   B[4]= A[1];
+   B[5]= A[1];
+   for(int d =0; d< N ;d++){
+    if (B[4] < A[d]){
+        B[4] = A[d];
+    }if(B[5] > A[d]){
+        B[5] = A[d];
+    }
+   }
+}
